bf442aecae310e37211dd94cad33353d7ee47c96
Yhh123jay <953191974@qq.com>
2024-01-08 09:31:51 +0800

    first commit

---
+++ src/main/java/com/yhh/springboot_hbase_ifc/SpringbootHbaseIfcApplication.java
@@ -0,0 +1,13 @@
+package com.yhh.springboot_hbase_ifc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHbaseIfcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHbaseIfcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yhh/springboot_hbase_ifc/SpringbootHbaseIfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhh.springboot_hbase_ifc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHbaseIfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
